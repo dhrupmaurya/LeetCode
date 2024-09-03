6f732be74cda85e815c6cabe53018ea3404924a6
Dhrup Chandra Maurya <124062249+dhrupmaurya@users.noreply.github.com>
2024-09-03 22:50:42 +0530

    Time: 0 ms, Memory: 7.7 MB - LeetHub

---
+++ implement-strstr/implement-strstr.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n=haystack.size();
+//         for(int i=0;i<n;i++){
+            
+//         }
+        return haystack.find(needle);
+    }
+};
