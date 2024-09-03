ed41ec538b96e31a024495d688cc968a5c8fe42e
Dhrup Chandra Maurya <124062249+dhrupmaurya@users.noreply.github.com>
2024-09-03 21:59:29 +0530

    Time: 8 ms, Memory: 13 MB - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head){
+            return NULL;
+        }
+        ListNode* cur=head, *prev=NULL;
+        while(cur){
+            ListNode *next1=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=next1;
+        }
+        return prev;
+    }
+};
+
